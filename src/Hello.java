7f0adf61d4459bed9dca85dc573a86a8341ef7bf
Lou <lou.contrucci@etu.univ-amu.fr>
2025-01-28 15:36:34 +0100

    je corrige mon prog

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hele world");// TODO Auto-generated method stub
+		System.out.println("hello world");// TODO Auto-generated method stub
 
 	}
 }
