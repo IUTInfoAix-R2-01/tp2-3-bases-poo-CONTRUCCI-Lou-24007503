a25bef8c58bb8d858020713497b1f9910b2a04dc
Lou <lou.contrucci@etu.univ-amu.fr>
2025-01-28 16:28:04 +0100

    Exercice 1-1, Main de Circle

---
+++ src/TestCircle.java
@@ -0,0 +1,15 @@
+//le renomer TestCircle pour que ca fonctionne
+public class TestCircle{
+	public static void main (String[] args) {
+		Circle c1 = new Circle();
+		System.out.println(" the circle has radius of "
+				+ c1.getRadius() + " and ara of " + c1.getArea());
+		
+		Circle c2 = new Circle(2.0);
+		
+		System.out.println(" the circle has radius of " 
+				+ c2.getRadius() + " and area of " + c2.getArea());
+		
+		
+	}
+}
